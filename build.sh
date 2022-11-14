c95d682d8e820d68cafd8189ad058ce5ec8b923a
Colin Davidson <colrdavidson@gmail.com>
2022-11-14 06:25:45 -0800

    make the shell / batch scripts match

--- build.sh
+++ build.sh
@@ -2,4 +2,11 @@ rm -rf bin
 mkdir bin
 
 cp fonts/* bin/.
-odin build src -collection:formats=formats -out:bin/spall -o:speed -debug -keep-temp-files -no-bounds-check
+
+if [ "$1"=="release" ]; then
+	odin build src -collection:formats=formats -out:bin/spall -debug -o:speed -no-bounds-check -define:GL_DEBUG=false
+elif [ "$1"=="opt" ]; then
+	odin build src -collection:formats=formats -out:bin/spall -debug -o:speed
+else
+	odin build src -collection:formats=formats -out:bin/spall -debug -keep-temp-files
+fi
